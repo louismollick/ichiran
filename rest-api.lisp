d81718e0d3008f39354944b7493bbbf90c48c202
Louis Mollick <louismollick@gmail.com>
2024-01-02 01:58:12 -0500

    feat: writing japanese sentence in path of server returns ichiran JSON, added env variables for PORT and LIMIT

--- rest-api.lisp
+++ rest-api.lisp
@@ -1,15 +1,23 @@
 (defpackage :ichiran/rest-api
-  (:use :cl)
-  (:export :start))
+ (:use :cl :ichiran/all)
+ (:export :start))
 
 (in-package :ichiran/rest-api)
 
-(defun handler (env) 
-  (declare (ignore env))
-  '(200 nil ("Hello World, redefinable!")))
+(defmethod jsown:to-json ((word-info word-info))
+ (jsown:to-json (word-info-gloss-json word-info)))
 
+(defun handler (env) 
+ (declare (ignore env))
+ (handler-case 
+  (destructuring-bind (&key path-info &allow-other-keys) env
+   (let ((input (subseq path-info 1))) ;; remove leading "/" from path
+    `(200 nil (,(jsown:to-json (romanize* input :limit (parse-integer (uiop:getenv "LIMIT"))))))))
+  (error (c)
+   `(400 nil (,(format nil "ERROR: ~a~%" c))))))
+    
 (defun start ()
-  (clack:clackup (lambda (env) (funcall 'handler env)) 
+ (clack:clackup (lambda (env) (funcall 'handler env)) 
   :address "0.0.0.0"
-  :port 5001
+  :port (parse-integer (uiop:getenv "PORT"))
   :use-thread nil))
